5f5a6c51c15af454c4eb10b7f62e88908a8f3a0e
FebriPratama13 <febripratama130205@gmail.com>
2025-06-02 14:47:06 +0700

    nama, nim Fixes #9

---
+++ lib/Profile/nama.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Nama extends StatefulWidget {
+  const Nama({super.key});
+
+  @override
+  State<Nama> createState() => _NamaState();
+}
+
+class _NamaState extends State<Nama> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
